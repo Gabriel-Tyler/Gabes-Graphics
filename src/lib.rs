5a4596fcbd5d5092a625a739ef619a873ef4531d
Gabe <gabriel.tyler16@gmail.com>
2024-08-08 19:25:43 -0500

    Add w coord test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
+    use nalgebra as na;
+    use approx::assert_relative_eq;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn point_and_vector_w_coordinate() {
+        let p = na::Point3::new(1.0, 2.0, 3.0);
+        let v = na::Vector3::new(1.0, 2.0, 3.0);
+        assert_relative_eq!(p.to_homogeneous(), na::Vector4::new(1.0, 2.0, 3.0, 1.0));
+        assert_relative_eq!(v.to_homogeneous(), na::Vector4::new(1.0, 2.0, 3.0, 0.0));
     }
 }
